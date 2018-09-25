262112ab819a638419dc157f8f2ba68bf46f36c9
MARY <mcruz2946@gmail.com>
2018-09-25 16:40:38 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -14,3 +14,12 @@ const codes = [
 function init() {
   // your code here
 }
+
+const alphbet = [ 'a', 'b', 'c',];
+let index = 0;
+function onKeyHandler(e) {const key = e.key;
+if (key === alphabet[index]) {index++;
+if (index === alphabet.length) {alert("hurray!");
+index = 0;}
+} else } index = 0;}
+
